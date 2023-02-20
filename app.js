8ecebebc90cd7824f33f8b7921a48759b6d2f7d2
Elena <kunstvanelena@gmail.com>
2023-02-20 15:38:46 +0100

    fixed controllWinner function with .every() method

--- app.js
+++ app.js
@@ -5,23 +5,15 @@ const startPage = document.querySelector('.start_page')
 const startBtn = document.querySelector('.start_btn')
 const gamePage = document.querySelector('.game_page')
 const gameBoard = document.querySelector('.game_board')
-const boardCell = document.querySelectorAll('.slot')
+const boardCell = [...document.querySelectorAll('.slot')] // Converting nodeList to an array
 const player0Display =document.querySelector('.player--0')
 const player1Display = document.querySelector('.player--1')
 const nameInput1 =document.querySelector('.name_input_0')
 const nameInput2 =document.querySelector('.name_input_1')
 const players = []
+let indexMarked_O = []
+let indexMarked_X = []
 let activePlayer = 0
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
 
 
 const Player = function(name, marker) {
@@ -30,7 +22,6 @@ const Player = function(name, marker) {
 }
 
 const switchPlayer = function () {
-
     // if player 0 is playing, give activePlayer to 1
     activePlayer = activePlayer === 0 ? 1 : 0
 
@@ -53,12 +44,28 @@ const renderPlayer = function () {
 }
 
 const startGame = function () {
+    // Hide start page and reveal game board
     startPage.style.display = 'none'
     gamePage.classList.remove('invisible')   
 }
 
 const controllWinner = function () {
-    
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let combi of winningConditions) {
+        let test = combi.every((item) => indexMarked_O.includes(item))
+        if(test) alert(`${players[1].name} won!`)
+    }
+
 }
 
 const Game = function () {
@@ -71,16 +78,28 @@ const Game = function () {
         renderPlayer()
     });
     
-    boardCell.forEach(cell => cell.addEventListener('click', () => {
-        if (activePlayer === 0) {
-            cell.textContent = players[0].marker
-        } else if (activePlayer === 1) {
-            cell.textContent = players[1].marker
-        };  
-        switchPlayer()
+    boardCell.forEach(cell => cell.addEventListener('click', (e) => {
+        if(cell.textContent===''){
+            if (activePlayer === 0) {
+                cell.textContent = players[0].marker            
+                indexMarked_O.push(+e.target.dataset.index)
+                
+                controllWinner()
+            } else if (activePlayer === 1) {
+                cell.textContent = players[1].marker
+                indexMarked_X.push(+e.target.dataset.index)
+
+            };  
+            switchPlayer()
+        }
     }))        
 }()    
 
+// const getIndex = function () {
+//     return boardCell
+//     .filter(cell => cell.textContent === 'O')
+//     .forEach(cell => (+cell.dataset.index))
+// }
 
 
 
